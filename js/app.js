8c8da543cfe6a365f896caef4a0aadb76eac4bd1
Elimane Wade <wadecheikh2994@gmail.com>
2025-01-18 21:06:13 +0100

    Modification egalement dans le main

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// document.addEventListener('scroll', function() {
+//     const sectionElement = document.querySelector('.section');
+//     const rect = sectionElement.getBoundingClientRect();
+//     const windowHeight = window.innerHeight;
+
+//     if (rect.top <= windowHeight - 100) {
+//         sectionElement.classList.add('visible');
+//     }
+// });
